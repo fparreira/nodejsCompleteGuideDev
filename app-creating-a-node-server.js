91d5516a990756d17e99d9c7228ab09e4452476c
Fernando Parreira <fparreira@gmail.com>
2023-06-05 22:30:52 -0300

    parsing request bodies

--- app-creating-a-node-server.js
+++ app-creating-a-node-server.js
@@ -1,9 +1,43 @@
 const http = require('http');
+const fs = require('fs');
 
 // console.log(http);
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</html>');
+        return res.end();
+    }
+
+     if(url === "/message" && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            // console.log(parseBody);
+            const message = parseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<h1>First Page Node Js Server</h1>');
+    res.write('</html>');
+    res.end();
+
 });
 
 server.listen(3000);
