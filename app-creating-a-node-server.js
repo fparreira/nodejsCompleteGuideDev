718ba0c4c8d92c1bab950e792e0e9598b7a31dae
Fernando Parreira <fparreira@gmail.com>
2023-06-06 14:44:07 -0300

    node js process control

--- app-creating-a-node-server.js
+++ app-creating-a-node-server.js
@@ -16,21 +16,24 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
 
-     if(url === "/message" && method === 'POST'){
+    if(url === "/message" && method === 'POST'){
         const body = [];
         req.on('data', (chunk) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parseBody = Buffer.concat(body).toString();
             // console.log(parseBody);
             const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
