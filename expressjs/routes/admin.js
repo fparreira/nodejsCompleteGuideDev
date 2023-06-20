ec19ef910d32a149c5ef080a5752d4dd5f9c9a09
Fernando Parreira <fparreira@gmail.com>
2023-06-20 15:48:32 -0300

    controllers init

--- expressjs/routes/admin.js
+++ expressjs/routes/admin.js
@@ -1,36 +1,21 @@
 const express = require('express');
 
-const path = require('path');
+const productsController = require('../controllers/products');
 
-const rootDir = require('../utils/path');
+// const path = require('path');
+
+// const rootDir = require('../utils/path');
 
 // console.log(rootDir);
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product -> GET
-router.get('/add-product', (req, res, next) => {
-    // console.log('In middleware product!');
-    // res.send('<form action="/admin/add-product" method="POST" ><input type="text" name="title"><button type="subtmit">send</button></form>')
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', {
-            pageTitle: 'Add Product',
-            path: '/admin/add-product',
-            formsCSS: true,
-            productCSS: true,
-            activeAddProduct: true
-        });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product -> POST
-router.post('/add-product', (req,res, next) => {
-    // console.log(req.body);
-    products.push({'title' : req.body.title});
-    res.redirect("/");
-});
+router.post('/add-product', productsController.postAddProduct);
 
-// module.exports = router;
-exports.routes = router;
-exports.products = products;
+module.exports = router;
+// exports.routes = router;
+// exports.products = products;
