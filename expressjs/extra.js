b8c0c502fb320d86589237aebeb7cb0858f849c0
Fernando Parreira <fparreira@gmail.com>
2023-06-29 13:46:26 -0300

    callback function

--- expressjs/extra.js
+++ expressjs/extra.js
@@ -1,23 +1,39 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.use('/fernando',(req, res) => {
-    res.send('Hello World');
-});
+// app.use('/fernando',(req, res) => {
+//     res.send('Hello World');
+// });
 
-// console.log(process.mainModule);
+// // console.log(process.mainModule);
 
-// console.log(require.main);
+// // console.log(require.main);
 
 
-let group = [];
-let name = 'fernando';
-let age = 46;
-let nameJsonStringify = JSON.stringify(name);
-let nameJsonParse = JSON.parse(nameJsonStringify);
+// let group = [];
+// let name = 'fernando';
+// let age = 46;
+// let nameJsonStringify = JSON.stringify(name);
+// let nameJsonParse = JSON.parse(nameJsonStringify);
 
-console.log(typeof nameJsonStringify);
-console.log(typeof nameJsonParse);
+// console.log(typeof nameJsonStringify);
+// console.log(typeof nameJsonParse);
 
-app.listen(3000);
+// app.listen(3000);
+
+function showNumbers(boolean, arg2, callback){
+
+    console.log('start asynchronous operation');
+
+    const message = 'the message is : ';
+
+    if (boolean) {
+        console.log('success !!' + arg2);
+    } else {
+        console.log('error !!');
+    } 
+
+}
+
+showNumbers(true, 'fernando');
